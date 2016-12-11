a8cf3579c8389710fbc2914aa6fd8e9f89601388
Jason Hanna <jason.hanna01@gmail.com>
2016-12-11 15:57:34 -0700

    add post show page

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS'
+export const FETCH_POST = 'FETCH_POST'
 export const CREATE_POSTS = 'CREATE_POSTS'
 
 const ROOT_URL = "http://reduxblog.herokuapp.com/api"
@@ -10,6 +11,14 @@ export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
   return{
     type: FETCH_POSTS,
+    payload: request
+  };
+}
+
+export function fetchPost(id) {
+  const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+  return{
+    type: FETCH_POST,
     payload: request
   };
 }
