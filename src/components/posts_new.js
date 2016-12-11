f95ef1df87fe5ee71d86ac64245a18f36d5080e9
Jason Hanna <jason.hanna01@gmail.com>
2016-12-10 17:07:30 -0700

    add validations to form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -8,17 +8,26 @@ class PostsNew extends Component {
     return (
       <form onSubmit={handleSubmit(this.props.createPosts)}>
         <h3>Create New Post</h3>
-        <div className='form-group'>
+        <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
           <label>Title</label>
           <input type='text' className='form-control' {...title}/>
+          <div className='form-control-label'>
+            {title.touched ? title.error : ''}
+          </div>
         </div>
-        <div className='form-group'>
+        <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
           <label>Categories</label>
           <input type='text' className='form-control' {...categories}/>
+          <div className='form-control-label'>
+            {categories.touched ? categories.error : ''}
+          </div>
         </div>
-        <div className='form-group'>
+        <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
           <label>Content</label>
           <textarea className='form-control' {...content}/>
+          <div className='form-control-label'>
+            {content.touched ? content.error : ''}
+          </div>
         </div>
         <button type='submit' className='btn btn-primary'>Submit Post</button>
       </form>
@@ -26,7 +35,21 @@ class PostsNew extends Component {
   }
 }
 
+function validate(values){
+  const errors = {}
+  if(!values.title){
+    errors.title = "You Must Enter a Title"
+  }
+  if(!values.categories){
+    errors.categories = "You Must Enter a Category"
+  }
+  if(!values.content){
+    errors.content = "You Must Enter some Content"
+  }
+  return errors
+}
+
 export default reduxForm({
   form: 'PostsNew',
-  fields: ['title', 'categories', 'content']
+  fields: ['title', 'categories', 'content'], validate
 }, null, {createPosts})(PostsNew)
